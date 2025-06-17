97c0d6e45caec0c95e01428401b974d0036f78a5
LucasBrito <luquinhasbritogba@hotmail.com>
2025-06-17 16:13:21 -0300

    refactor: remover controladores, modelos, requisições, serviços e seeders do domínio BlogComplete

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
 
-require __DIR__ . '/roles.php';
+require __DIR__.'/roles.php';
 
-require __DIR__ . '/permissions.php';
+require __DIR__.'/permissions.php';
 
-require __DIR__ . '/domains/users.php';
-
-require __DIR__ . '/domains/blog-complete.php';
+require __DIR__.'/domains/users.php';
